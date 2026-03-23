787cf10e4fabc229a06e1e17178eb644c63cc13a
Jhonito021 <jhonito021@gmail.com>
2026-03-23 23:31:50 +0300

    initialisation des composant

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Home from "./components/home/home";
 
 
 function App() {
-  
+    return (
+        <>
+            <Home />
+        </>
+    )
 };
 
 export default App;
