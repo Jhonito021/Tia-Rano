334f0247a1fba2ac0a7e3fb8b9732bdba13fa5e0
Jhonito021 <jhonito021@gmail.com>
2026-03-28 21:52:09 +0300

    debug

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,11 +3,7 @@ import './home.css';
 function Home () {
     return (
         <div className="home container">
-                <ul>
-                    <li className="nav-link">
-                        
-                    </li>
-                </ul>
+            
         </div>
     )
 }
